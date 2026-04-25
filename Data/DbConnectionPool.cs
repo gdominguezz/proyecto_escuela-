d267c1d7cc1042e4adeadac278d994e135ebd22c
giovanix50 <tristan.dominguez.2004@gmail.com>
2026-04-24 23:36:24 -0600

    x

--- Data/DbConnectionPool.cs
+++ Data/DbConnectionPool.cs
@@ -1,6 +1,22 @@
-﻿namespace proyecto_escuela.Data
+﻿using Npgsql;
+
+namespace proyecto_escuela.Data;
+
+public class DbConnectionPool
 {
-    public class DbConnectionPool
+    private readonly NpgsqlDataSource _dataSource;
+
+    public DbConnectionPool(IConfiguration config)
     {
+        var connStr = config.GetConnectionString("Postgres")
+                     ?? throw new InvalidOperationException("Falta ConnectionStrings:Postgres en appsettings.json");
+
+        _dataSource = NpgsqlDataSource.Create(connStr);
     }
+
+    public async Task<NpgsqlConnection> OpenAsync()
+        => await _dataSource.OpenConnectionAsync();
+
+    public NpgsqlConnection Open()
+        => _dataSource.OpenConnection();
 }
